914e0ab01efc224eb05ece0bcbb45fb89f570346
JoeIt <josignaciot@gmail.com>
2023-03-06 11:38:41 -0300

    Refactoring

--- build/index.js
+++ build/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
+const MatchReader_1 = require("./MatchReader");
 const Matchresults_1 = require("./Matchresults");
-const reader = new CsvFileReader_1.CsvFileReader('football.csv');
-reader.read();
-console.log(reader.data);
+const CsvFileReader_1 = require("./CsvFileReader");
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// Create an instance of MatchReader and pass in something satisfying the 'Data Reader' interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === Matchresults_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
